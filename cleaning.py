395c3d4a158dbab9d0e37306748cadab74771197
sakabmw <sakabumi.wahyudi@gmail.com>
2025-01-14 12:56:01 +0700

    create data cleaning file

---
+++ cleaning.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import re
+
+def data_cleaning(csv_input, csv_output):
+    # Load the raw data
+    df = pd.read_csv(csv_input)
+
+    # Clean the values in column `quote`
+    df['quote'] = [re.sub(r'[“”.,]', '', quote) for quote in df['quote']]
+    # df['quote'] = df['quote'].apply(lambda x: re.sub(r'[“”.,]', '', x))
+
+    # Save the cleaned data into a new CSV
+    df.to_csv(csv_output, index=False)
