19f204caa99036c608e43d32140c574af9fd6428
sakabmw <sakabumi.wahyudi@gmail.com>
2025-01-14 17:02:21 +0700

    refine scraping flow

--- data_scraping.py
+++ data_scraping.py
@@ -3,9 +3,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import re
 
-# URL of the website to scrape
-url_base = "http://quotes.toscrape.com/"
-
 # List to store scraped data
 list_quote = []
 
@@ -52,7 +49,8 @@ def data_scraper(url):
         print(f"=== Failed to retrieve page: {url} ===")
 
     print("=== Scraping completed. ===")
-    print("=== Start storing the scraped data into a CSV file. ===")
+    print("=== Create a dataframe to store the scraped data. ===")
     df = pd.DataFrame(list_quote)
-    df.to_csv('quotes_raw.csv', index=False)
-    print("Done. Data saved to 'quotes_raw.csv'.")
+    print("=== The data have been stored in the dataframe. ===")
+    
+    return df
